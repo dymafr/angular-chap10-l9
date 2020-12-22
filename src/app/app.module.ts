b05eb71d73d5b3485b629fbccde6b6c127e3c2ef
dymafr <erwan1309@hotmail.com>
2020-12-22 11:41:25 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { UsersComponent } from "./users/users.component";
 import { UserComponent } from "./user/user.component";
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const APP_ROUTES: Routes = [
   { path: "", component: HomepageComponent },
@@ -22,7 +23,8 @@ const APP_ROUTES: Routes = [
     AppComponent,
     HomepageComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    NotFoundComponent
   ],
   bootstrap: [AppComponent],
   providers: []
