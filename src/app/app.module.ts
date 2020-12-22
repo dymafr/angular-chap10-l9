6de33195ce6d3e8a4fd390fc64e41f63221ee6d1
dymafr <erwan1309@hotmail.com>
2020-12-22 11:42:37 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { UsersComponent } from "./users/users.component";
 import { UserComponent } from "./user/user.component";
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 const APP_ROUTES: Routes = [
   { path: "", component: HomepageComponent },
@@ -14,7 +14,8 @@ const APP_ROUTES: Routes = [
     path: "users",
     component: UsersComponent,
     children: [{ path: ":id", component: UserComponent }]
-  }
+  },
+  { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
